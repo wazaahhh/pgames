fbd4308119f3cb844019317dc0a3b4f35e61fc0b
Thomas Maillart <thomas.maillart@gmail.com>
2015-09-08 08:25:42 -0700

    fixed plots to the new data structure

--- simulations/plot_abm.py
+++ simulations/plot_abm.py
@@ -6,14 +6,89 @@ from plotly.graph_objs import *
 from analyze_abm import *
 from parseFiles import *
 
-    
-def prepareXYZ(descDic):
-    
-    parDic = cooperationLevel(descDic)
-    x = np.array(parDic['d'])
-    y = np.array(parDic['s'])
-    z = np.array(parDic['coopLevel'])
 
+def loadFinalCoopStates(SummaryAllDic, mRange="all"):
+    
+    d = []
+    M = []
+    s = []
+    c = []
+    
+    for line in SummaryAllDic:
+        
+        if float(line['input']['iter']) < 2000 and float(line['input']['M']) == 1:
+            continue
+        
+        if not mRange=="all" and float(line['input']['M']) != mRange:
+            continue
+        
+        d = np.append(d,float(line['input']['d']))
+        M = np.append(M,float(line['input']['M']))
+        s = np.append(s,float(line['input']['s']))
+        c = np.append(c,float(line['summary'][-1][2]))
+
+
+    return {'d':d,'M':M,'s':s,'c':c}
+    
+
+def plotContours(SummaryAllDic):
+    dic = loadFinalCoopStates(SummaryAllDic)
+
+    colorDic = {1:'y',2:'g',5:'r',7:'b',11:'k'}
+    
+    pl.close("all")
+    pl.figure(1,(9,9))
+    for i,M in enumerate(np.unique(dic['M'])):
+        
+        if float(M) in [3,4]:
+            continue
+        
+        xMin = []
+        yMin = []
+        
+        yMedian = []
+        
+        xMax = []
+        yMax = []
+         
+        for density in np.unique(dic['d']):
+            indexMaxSusCoop = (dic['d']==density)*(dic['M']==M)*(dic['c'] >= 0.50)
+            indexMinUnSusCoop = (dic['d']==density)*(dic['M']==M)*(dic['c'] < 0.50)
+            if len(dic['s'][indexMaxSusCoop]) == 0 or len(dic['s'][indexMinUnSusCoop]) == 0:
+                print density,M," no data"
+                continue
+            
+            ymin = np.min(dic['s'][indexMinUnSusCoop])
+            ymax = np.max(dic['s'][indexMaxSusCoop])
+            
+            indexBetween = (dic['d']==density)*(dic['M']==M)*(dic['s'] >= ymin)*(dic['s'] <= ymax)
+            ymedian = np.median(dic['s'][indexBetween])
+            
+            print "d=%s, M=%s : " %(density,M), ymin,ymedian,ymax
+            xMin.append(density)
+            yMin.append(ymin)
+            
+            yMedian.append(ymedian)
+            
+            xMax.append(density)
+            yMax.append(ymax)
+
+        #pl.plot(xMin,yMin,color=colorDic[M])
+        pl.plot(xMax,yMedian,"-",label= "M=%s"%str(M),color=colorDic[M])
+        pl.fill_between(xMin,yMin,yMax,label="M=%s"%str(M),alpha=0.2,color=colorDic[M])
+        
+    pl.xlabel("population density")
+    pl.ylabel("property violation")
+    pl.xlim(xmin=0.2) 
+    pl.ylim(0,0.08)
+    pl.legend(loc=0)
+    
+
+def prepareXYZ(dicArrays):
+    
+    x = np.array(dicArrays['d'])
+    y = np.array(dicArrays['s'])
+    z = np.array(dicArrays['c'])
     
     #index = np.argwhere(np.array(parDic['M'])==M)
     #print index
@@ -40,10 +115,8 @@ def prepareXYZ(descDic):
 
 
 
-def export2plotly(descDic):
+def export2plotly(dicXYZ):
         
-    dicXYZ = prepareXYZ(descDic)  
-
     data = Data([
         Surface(
             z = dicXYZ['Z'],
@@ -54,7 +127,7 @@ def export2plotly(descDic):
         )
     ])
     layout = Layout(
-        title = descDic.__str__(),
+        #title = descDic.__str__(),
         autosize = False,
         width = 1500,
         height = 900,
@@ -176,10 +249,9 @@ def export2plotly2(X,Y,Z):
     plot_url = py.plot(fig)
     return plot_url
 
-def heatmap(descDic):
+def heatmap(dic):
     
-    parDic = cooperationLevel(descDic)
-    x = np.array(parDic['d'])
+    x = np.array(dic['d'])
     y = np.array(parDic['s'])
     z = np.array(parDic['coopLevel'])
 
